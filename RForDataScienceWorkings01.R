7666c826a175594c920e6feced47939698929c4a
rtse999 <rtse999@gmail.com>
2017-11-24 23:51:53 +1100

    23:51 - Friday 24 November 2017

--- RForDataScienceWorkings01.R
+++ RForDataScienceWorkings01.R
@@ -18,6 +18,9 @@ library(hexbin)
 library(modelr)
 library(stringr)
 library(forcats)
+library(lubridate)
+
+devtools::session_info()
 
 mpg
 
@@ -619,6 +622,41 @@ relig <- gss_cat %>%
 ggplot(relig, aes(tvhours, relig)) +
   geom_point()
 
+ggplot(relig, aes(tvhours, fct_reorder(relig, tvhours))) +
+  geom_point()
 
+# Dates and times with lubridate
+today()
+now()
 
+ymd("2017-11-24")
+ymd(20171212)
 
+flights %>% 
+  select(year, month, day, hour, minute) %>% 
+  mutate(
+    departure = make_datetime(year, month, day, hour, minute)
+  )
+
+make_datetime_100 <- function(year, month, day, time) {
+  make_datetime(year, month, day, time %/% 100, time %% 100)
+}
+
+flights_dt <- flights %>% 
+  filter(!is.na(dep_time), !is.na(arr_time)) %>% 
+  mutate(
+    dep_time = make_datetime_100(year, month, day, dep_time),
+    arr_time = make_datetime_100(year, month, day, arr_time),
+    sched_dep_time = make_datetime_100(year, month, day, sched_dep_time),
+    sched_arr_time = make_datetime_100(year, month, day, sched_arr_time)
+  ) %>% 
+  select(origin, dest, ends_with("delay"), ends_with("time"))
+  
+flights_dt %>% 
+  ggplot(aes(dep_time)) +
+  geom_freqpoly(binwidth = 86400)
+
+flights_dt %>% 
+  filter(dep_time < ymd(20130102)) %>% 
+  ggplot(aes(dep_time)) +
+  geom_freqpoly(binwidth = 600)
