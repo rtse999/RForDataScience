c1f787ff6aca615d800205e191bf3258fbefdeae
rtse999 <rtse999@gmail.com>
2018-02-24 11:12:31 +1100

    11:12 - Saturday 24 February 2018

--- RForDataScienceWorkings01.R
+++ RForDataScienceWorkings01.R
@@ -17,22 +17,22 @@ format(Sys.time(), "%a %b %d %H:%M:%S %Y")
 # Install Packages
 # ------------------------------------------------------------------------
 #install.packages("hexbin")
-library(tidyverse)
-library(nycflights13)
-library(ggstance)
-library(lvplot)
-library(hexbin)
-library(modelr)
-library(stringr)
+library(dplyr)
 library(forcats)
+library(ggstance)
+library(hexbin)
 library(lubridate)
+library(lvplot)
 library(magrittr)
+library(modelr)
+library(nycflights13)
 library(purrr)
 library(splines)
+library(stringr)
+library(tidyverse)
 
+options(na.action = na.warn)
 devtools::session_info()
-
-mpg
 
 # ------------------------------------------------------------------------
 # ggplot
@@ -1008,3 +1008,44 @@ ggplot(sim5, aes(x,y)) +
   geom_point() +
   geom_line(data = grid, colour = "red") +
   facet_wrap(~ model)
+
+# ------------------------------------------------------------------------
+# Chapter 19: Model building
+# ------------------------------------------------------------------------
+ggplot(diamonds, aes(cut, price)) + geom_boxplot()
+ggplot(diamonds, aes(color, price)) + geom_boxplot()
+ggplot(diamonds, aes(clarity, price)) + geom_boxplot()
+ggplot(diamonds, aes(carat, price)) + geom_hex(bins = 50)
+
+diamonds2 <- diamonds %>%
+  filter(carat <= 2.5) %>% 
+  mutate(lprice = log2(price), lcarat = log2(carat))
+
+ggplot(diamonds2, aes(lcarat, lprice)) +
+  geom_hex(bins = 50)
+
+mod_diamond <- lm(lprice ~ lcarat, data = diamonds2)
+
+grid <- diamonds2 %>% 
+  data_grid(carat = seq_range(carat, 20)) %>%
+  mutate(lcarat = log2(carat)) %>% 
+  add_predictions(mod_diamond, "lprice") %>% 
+  mutate(price = 2 ^ lprice)
+
+ggplot(diamonds2, aes(carat, price)) +
+  geom_hex(bins = 50) +
+  geom_line(data = grid, colour = "red", size = 1)
+
+diamonds2 <- diamonds2 %>% 
+  add_residuals(mod_diamond, "lresid")
+
+ggplot(diamonds2, aes(lcarat, lresid)) +
+  geom_hex(bins = 50)
+
+ggplot(diamonds2, aes(cut, lresid)) + geom_boxplot()
+ggplot(diamonds2, aes(color, lresid)) + geom_boxplot()
+ggplot(diamonds2, aes(clarity, lresid)) + geom_boxplot()
+
+mod_diamond2 <- lm(lprice ~ lcarat + color + cut + clarity,
+                   data = diamonds2)
+
