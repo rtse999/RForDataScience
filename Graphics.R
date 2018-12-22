9df7b7d35d67eac90cd28a8d5b6bfd78d8e2524f
rtse999 <rtse999@gmail.com>
2018-12-23 00:02:33 +1100

    00:02 - Sunday 23 December 2018

--- Graphics.R
+++ Graphics.R
@@ -26,11 +26,14 @@ library(tidyverse)
 mpg
 skim(mpg)
 
+mtcars
+skim(mtcars)
+
 # Basic plot
 ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy))
 
-# Exercises 
+# Exercises: The mpg data frame
 #1. 
 ggplot(data = mpg)
 
@@ -45,3 +48,65 @@ ncol(mtcars)
 ggplot(data = mpg) +
   geom_point(mapping = aes(x = cyl, y = hwy))
 
+# Aesthetic mappings
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, colour = class))
+
+ggplot(mpg) +
+  geom_point(aes(x = displ, y = hwy, size = class))
+
+ggplot(mpg) +
+  geom_point(aes(x = displ, y = hwy, alpha = class))
+
+ggplot(mpg) +
+  geom_point(aes(x = displ, y = hwy, shape = class))
+
+ggplot(mpg) +
+  geom_point(aes(x = displ, y = hwy), colour = "blue")
+
+ggplot(mpg) +
+  geom_point(aes(x = displ, y = hwy, colour = displ < 5))
+
+ggplot(mtcars) +
+  geom_point(aes(x = wt, y = hp))
+
+ggplot(mtcars) +
+  geom_histogram(aes(x = drat))
+
+ggplot(diamonds, aes(carat)) +
+  geom_histogram()
+
+# From: https://www.r-bloggers.com/how-to-make-a-histogram-with-ggplot2/
+
+chol <- read.table(url("http://assets.datacamp.com/blog_assets/chol.txt"), header = TRUE)
+
+ggplot(data=chol, aes(chol$AGE)) + 
+  geom_histogram(breaks=seq(20, 50, by = 2), 
+                 col="red", 
+                 fill="green", 
+                 alpha = .2) + 
+  labs(title="Histogram for Age") +
+  labs(x="Age", y="Count") + 
+  xlim(c(18,52)) + 
+  ylim(c(0,30))
+
+ggplot(data=chol, aes(chol$AGE)) + 
+  geom_histogram(boundary = 0)
+
+ggplot(data=chol, aes(chol$AGE)) + 
+  geom_histogram(breaks=seq(20, 50, by = 1))
+
+sum(chol$AGE < 22)
+
+tmp <- 
+  dplyr::filter(chol, AGE <= 22) %>% 
+  arrange(AGE)
+
+ggplot(tmp) +
+  geom_histogram(aes(x = AGE), bins = 5)
+
+ggplot(tmp) +
+  geom_histogram(aes(x = AGE), bins = 30)
+
+ggplot(tmp) +
+  geom_histogram(aes(x = AGE), breaks = seq(18, 22, by = 1))
