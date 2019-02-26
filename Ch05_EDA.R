4f9b0c5c9143029d8ff00117c49865d721624656
rtse999 <rtse999@gmail.com>
2019-02-26 23:29:15 +1100

    23:29 - Tuesday 26 February 2019

--- Ch05_EDA.R
+++ Ch05_EDA.R
@@ -45,4 +45,40 @@ smaller %>%
   ggplot(aes(x = carat)) +
   geom_histogram(binwidth = 0.1)
 
+smaller %>% 
+  ggplot(aes(x = carat, colour = cut)) +
+  geom_freqpoly(binwidth = 0.1)
 
+smaller %>%
+  ggplot(aes(x = carat)) +
+  geom_histogram(binwidth = 0.01)
+
+diamonds %>% 
+  ggplot(aes(x = y)) +
+  geom_histogram(binwidth = 0.5) + 
+  coord_cartesian(ylim = c(0, 50))
+
+unusual <-
+  diamonds %>% 
+  dplyr::filter(y < 3 | y > 20) %>% 
+  arrange(y)
+
+diamonds %>% 
+  ggplot(aes(x = price)) +
+  geom_histogram(binwidth = 1000)
+
+diamonds %>% 
+  count(cut_width(price, 1000))
+
+diamonds %>% 
+  ggplot(aes(x = carat)) +
+  geom_histogram(binwidth = 0.01) +
+  coord_cartesian(xlim = c(0.9, 1.1))
+
+diamonds %>% 
+  ggplot() +
+  geom_count(aes(x = cut, y = color))
+
+diamonds %>%
+  count(color, cut)
+  
